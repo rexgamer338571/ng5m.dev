f1e8bc5c07c7b0d3f4d47c5e19e7fcf21e92e0dc
rexgamer338571 <wojciechm225@gmail.com>
2024-10-27 00:26:19 +0200

    wow effect

--- script.js
+++ script.js
@@ -49,4 +49,20 @@ function switchMode() {
 `--hover: ${dict["hover"]};\n`;
 }
 
+function makeNet() {
+    VANTA.NET({
+        el: "#net",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: darkMode["text"],
+        backgroundColor: darkMode["bgcolor"]
+    });
+}
+
+makeNet();
 juggleHeaderText();
